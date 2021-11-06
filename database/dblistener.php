f93a3e18fda24ba458aa5c4a976fdaa957636024
Jamison <jc859@njit.edu>
2021-11-06 17:38:05 -0400

    added event logger

--- database/dblistener.php
+++ database/dblistener.php
@@ -5,6 +5,12 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('login.php.inc');
 require_once('register.php.inc');
+require_once('event_logger.php');
+
+function doLog($error){
+	file_put_contents("hublog.txt", $error, FILE_APPEND);
+}
+
 
 function doLogin($username,$password)
 {
@@ -27,6 +33,8 @@ function requestProcessor($request)
   var_dump($request);
   if(!isset($request['type']))
   {
+	$event = date("Y-m-d") . "  " . date("h:i:sa") . " --- DataBase --- " . "ERROR: unsupported message type" . "\n";
+    log_event($event);
     return "ERROR: unsupported message type";
   }
   switch ($request['type'])
@@ -37,11 +45,17 @@ function requestProcessor($request)
       return doValidate($request['sessionId']);
     case "register":
       return doRegister($request['username'],$request['password'],$request['email']);
+    case "event_log":
+      doLog($request['error_message']);
+    /*default:
+      $event = date("Y-m-d") . "  " . date("h:i:sa") . " --- Database --- " . "Server received request but request type does not match" . "\n";
+      log_event($event);
+      */
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+$server = new rabbitMQServer("database.ini","testServer");
 
 $server->process_requests('requestProcessor');
 exit();
---
+++ database/event_logger.php
@@ -0,0 +1,30 @@
+<?php	
+    	//Error logging
+    	error_reporting(E_ALL);    
+    	ini_set('display_errors', 'On');
+    	ini_set('log_errors', 'On');
+    	
+    function log_event($event_string){
+    	//append error to log file
+    	file_put_contents("hublog.txt", $event_string, FILE_APPEND);
+    	
+    	//making error message request
+    	$request = array();
+    	$request['type'] = "event_log";
+    	$request['error_message'] = $event_string;
+    	
+    	
+    	//send error to frontend server
+    	$db = new rabbitMQClient("frontend.ini","testServer");
+    	$response = $db->publish($request);
+    	/*
+    	//send error to dmz server
+    	$dmz = new rabbitMQClient("dmz.ini","testServer");
+    	$response = $dmz->publish($request);
+    	*/
+    	//send error to RabbitMQ server
+    	$rbmq = new rabbitMQClient("rabbit.ini","testServer");
+    	$response = $rbmq->publish($request);
+    	
+    }
+?>
