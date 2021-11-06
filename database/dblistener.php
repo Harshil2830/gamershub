a64c52783e484805be941c9453cbc86c2f143e5b
Jamison <jc859@njit.edu>
2021-11-06 15:28:59 -0400

    first commit

---
+++ database/dblistener.php
@@ -0,0 +1,49 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('login.php.inc');
+require_once('register.php.inc');
+
+function doLogin($username,$password)
+{
+    // lookup username in databas
+    // check password
+    $login = new loginDB();
+    return $login->validateLogin($username,$password);
+    //return false if not valid
+}
+
+
+function doregister($username,$password,$email)
+{
+    $login = new registerDB();
+    return $login->validateregister($username,$password,$email);
+}
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+    case "register":
+      return doRegister($request['username'],$request['password'],$request['email']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+$server->process_requests('requestProcessor');
+exit();
+?>
+
