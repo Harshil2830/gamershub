ac3244505d99e90ce9219be157360440e5cc4a05
superalik123 <44001727+superalik123@users.noreply.github.com>
2021-11-09 11:29:50 -0500

    Add files via upload

---
+++ rabbitmq/emailscript.py
@@ -0,0 +1,41 @@
+import smtplib
+from email.message import EmailMessage
+
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+
+    user = "gamershubtest@gmail.com"
+    msg['from'] = user
+    password = "rzxdnjillyjvcuar"
+
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login(user,password)
+    server.send_message(msg)
+    server.quit()
+
+if __name__ == '__main__':
+   email_alert("Gamers Hub Notification","Welcome to gamers hub!","akhan1337@hotmail.com")
+content = 'example'
+
+def email_signup(username, email):
+    msg = EmailMessage()
+    body = "Welcome "+username+","+"\n\nYour account was succesfully created!"
+    msg.set_content(body)
+    msg['subject'] = "Welcome to the Game Cave!"
+    msg['to'] = email
+
+    user = "gamershubtest@gmail.com"
+    msg['from'] = user
+    password = "rzxdnjillyjvcuar"
+
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login(user,password)
+    server.send_message(msg)
+    server.quit()
+
+email_signup("Akhan1337", "akhan1337@hotmail.com")
